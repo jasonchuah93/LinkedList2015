35754a9cb2eadbff841ac1e221d78bdbeb41e9dd
jasonchuah93 <jasonchuah1993@hotmail.com>
2015-07-05 22:36:22 +0800

    Added elements into linkedList using listAddlast

--- test/test_LinkedList.c
+++ test/test_LinkedList.c
@@ -233,6 +233,9 @@ void test_listAddLast_should_add_elements_into_linkedList(){
 	LinkedList *list = createLinkedList();
 	Element ele1 = {.next = (Element *)NULL, .data = (Element *)1};
 	Element ele2 = {.next = (Element *)NULL, .data = (Element *)5};
+	Element ele3 = {.next = (Element *)NULL, .data = (Element *)10};
+	Element ele4 = {.next = (Element *)NULL, .data = (Element *)15};
+	Element ele5 = {.next = (Element *)NULL, .data = (Element *)20};
 	
 	listAddLast(&ele1,list);
 	TEST_ASSERT_EQUAL(1,ele1.data);
@@ -245,6 +248,38 @@ void test_listAddLast_should_add_elements_into_linkedList(){
 	
 	listAddLast(&ele2,list);
 	TEST_ASSERT_EQUAL(5,ele2.data);
+	TEST_ASSERT_EQUAL(1,list->head->data);
+	TEST_ASSERT_EQUAL(5,list->tail->data);
+	TEST_ASSERT_NOT_EQUAL(list->head,list->tail);
+	TEST_ASSERT_EQUAL(2,list->length);
+	TEST_ASSERT_NULL(list->head->next);
+	TEST_ASSERT_NOT_NULL(list->tail->next);
 	
+	listAddLast(&ele3,list);
+	TEST_ASSERT_EQUAL(10,ele3.data);
+	TEST_ASSERT_EQUAL(1,list->head->data);
+	TEST_ASSERT_EQUAL(10,list->tail->data);
+	TEST_ASSERT_NOT_EQUAL(list->head,list->tail);
+	TEST_ASSERT_EQUAL(3,list->length);
+	TEST_ASSERT_NULL(list->head->next);
+	TEST_ASSERT_NOT_NULL(list->tail->next);
+	
+	listAddLast(&ele4,list);
+	TEST_ASSERT_EQUAL(15,ele4.data);
+	TEST_ASSERT_EQUAL(1,list->head->data);
+	TEST_ASSERT_EQUAL(15,list->tail->data);
+	TEST_ASSERT_NOT_EQUAL(list->head,list->tail);
+	TEST_ASSERT_EQUAL(4,list->length);
+	TEST_ASSERT_NULL(list->head->next);
+	TEST_ASSERT_NOT_NULL(list->tail->next);
+	
+	listAddLast(&ele5,list);
+	TEST_ASSERT_EQUAL(20,ele5.data);
+	TEST_ASSERT_EQUAL(1,list->head->data);
+	TEST_ASSERT_EQUAL(20,list->tail->data);
+	TEST_ASSERT_NOT_EQUAL(list->head,list->tail);
+	TEST_ASSERT_EQUAL(5,list->length);
+	TEST_ASSERT_NULL(list->head->next);
+	TEST_ASSERT_NOT_NULL(list->tail->next);
 }
 
